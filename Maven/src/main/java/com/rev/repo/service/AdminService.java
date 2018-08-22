4e0e07cdfa1fad47830dae9c8f36b7a90a30afb8
unknown <hcthamas@gmail.com>
2018-08-22 11:29:35 -0500

    Admin controller and service init

---
+++ Maven/src/main/java/com/rev/repo/service/AdminService.java
@@ -0,0 +1,16 @@
+package com.rev.repo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+//	1) admin can edit users
+//	2)  admin delete users
+//	3) admin can delete post
+//	4) admin can edit post
+//	5) promote users to admin
+//	6) create user accounts / admin account // radio to distinguish type
+//	7) view flagged post
+	
+	
+}
